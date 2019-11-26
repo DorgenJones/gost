f507b27d95c7fd6ed574dad6bcf9ad7d01e7aabd
Zamperini <dblpfilter@163.com>
2019-11-26 11:28:40 +0800

    通信层

--- bytes/buffer.go
+++ bytes/buffer.go
@@ -1,39 +1,23 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-// Copy from go sdk 1.12.4 and change
+// Copy from types.Buffer (go@1.12.4) and change
 // 1. clean useless code
 // 2. change the process of ReadFrom
 
 package gxbytes
-
-// Simple byte buffer for marshaling data.
 
 import (
 	"errors"
 	"io"
 )
 
-// A ByteBuffer is a variable-sized buffer of bytes with Read and Write methods.
-// The zero value for ByteBuffer is an empty buffer ready to use.
 type ByteBuffer struct {
-	buf       []byte   // contents are the bytes buf[off : len(buf)]
-	off       int      // read at &buf[off], write at &buf[len(buf)]
-	bootstrap [64]byte // memory to hold first slice; helps small buffers avoid allocation.
-	lastRead  readOp   // last read operation, so that Unread* can work correctly.
-	copy      *[]byte
-	// FIXME: it would be advisable to align ByteBuffer to cachelines to avoid false
-	// sharing.
+	buf      []byte
+	off      int
+	lastRead readOp
+	copy     *[]byte
 }
 
-// The readOp constants describe the last action performed on
-// the buffer, so that UnreadRune and UnreadByte can check for
-// invalid usage. opReadRuneX constants are chosen such that
-// converted to int they correspond to the rune size that was read.
 type readOp int8
 
-// Don't use iota for these, as the values need to correspond with the
-// names and comments, which is easier to see when being explicit.
 const (
 	opRead      readOp = -1 // Any other read operation.
 	opInvalid   readOp = 0  // Non-read operation.
@@ -43,23 +27,13 @@ const (
 	opReadRune4 readOp = 4  // Read rune of size 4.
 )
 
-// ErrTooLarge is passed to panic if memory cannot be allocated to store data in a buffer.
 var ErrTooLarge = errors.New("bytes.ByteBuffer: too large")
 var errNegativeRead = errors.New("bytes.ByteBuffer: reader returned negative count from Read")
 
 const maxInt = int(^uint(0) >> 1)
 
-// Bytes returns a slice of length b.Len() holding the unread portion of the buffer.
-// The slice is valid for use only until the next buffer modification (that is,
-// only until the next call to a method like Read, Write, Reset, or Truncate).
-// The slice aliases the buffer content at least until the next buffer modification,
-// so immediate changes to the slice will affect the result of future reads.
 func (b *ByteBuffer) Bytes() []byte { return b.buf[b.off:] }
 
-// String returns the contents of the unread portion of the buffer
-// as a string. If the ByteBuffer is a nil pointer, it returns "<nil>".
-//
-// To build strings more efficiently, see the strings.Builder type.
 func (b *ByteBuffer) String() string {
 	if b == nil {
 		// Special case, useful in debugging.
@@ -68,20 +42,12 @@ func (b *ByteBuffer) String() string {
 	return string(b.buf[b.off:])
 }
 
-// empty returns whether the unread portion of the buffer is empty.
 func (b *ByteBuffer) empty() bool { return len(b.buf) <= b.off }
 
-// Len returns the number of bytes of the unread portion of the buffer;
-// b.Len() == len(b.Bytes()).
 func (b *ByteBuffer) Len() int { return len(b.buf) - b.off }
 
-// Cap returns the capacity of the buffer's underlying byte slice, that is, the
-// total space allocated for the buffer's data.
 func (b *ByteBuffer) Cap() int { return cap(b.buf) }
 
-// Truncate discards all but the first n unread bytes from the buffer
-// but continues to use the same allocated storage.
-// It panics if n is negative or greater than the length of the buffer.
 func (b *ByteBuffer) Truncate(n int) {
 	if n == 0 {
 		b.Reset()
@@ -94,9 +60,6 @@ func (b *ByteBuffer) Truncate(n int) {
 	b.buf = b.buf[:b.off+n]
 }
 
-// Reset resets the buffer to be empty,
-// but it retains the underlying storage for use by future writes.
-// Reset is the same as Truncate(0).
 func (b *ByteBuffer) Reset() {
 	b.buf = b.buf[:0]
 	b.off = 0
@@ -110,9 +73,6 @@ func (b *ByteBuffer) Free() {
 	}
 }
 
-// tryGrowByReslice is a inlineable version of grow for the fast-case where the
-// internal buffer only needs to be resliced.
-// It returns the index where bytes should be written and whether it succeeded.
 func (b *ByteBuffer) tryGrowByReslice(n int) (int, bool) {
 	if l := len(b.buf); n <= cap(b.buf)-l {
 		b.buf = b.buf[:l+n]
@@ -125,9 +85,6 @@ func (b *ByteBuffer) Name() string {
 	return "ByteBuffer"
 }
 
-// grow grows the buffer to guarantee space for n more bytes.
-// It returns the index where bytes should be written.
-// If the buffer can't grow it will panic with ErrTooLarge.
 func (b *ByteBuffer) grow(n int) int {
 	m := b.Len()
 	// If buffer is empty, reset to recover space.
@@ -137,12 +94,6 @@ func (b *ByteBuffer) grow(n int) int {
 	// Try to grow by means of a reslice.
 	if i, ok := b.tryGrowByReslice(n); ok {
 		return i
-	}
-	// Check if we can make use of bootstrap array.
-	if b.buf == nil && n <= len(b.bootstrap) {
-		b.buf = b.bootstrap[:n]
-		b.copy = &b.buf
-		return 0
 	}
 	c := cap(b.buf)
 	if n <= c/2-m {
@@ -167,11 +118,6 @@ func (b *ByteBuffer) grow(n int) int {
 	return m
 }
 
-// Grow grows the buffer's capacity, if necessary, to guarantee space for
-// another n bytes. After Grow(n), at least n bytes can be written to the
-// buffer without another allocation.
-// If n is negative, Grow will panic.
-// If the buffer can't grow it will panic with ErrTooLarge.
 func (b *ByteBuffer) Grow(n int) {
 	if n < 0 {
 		panic("bytes.ByteBuffer.Grow: negative count")
@@ -180,9 +126,6 @@ func (b *ByteBuffer) Grow(n int) {
 	b.buf = b.buf[:m]
 }
 
-// Write appends the contents of p to the buffer, growing the buffer as
-// needed. The return value n is the length of p; err is always nil. If the
-// buffer becomes too large, Write will panic with ErrTooLarge.
 func (b *ByteBuffer) Write(p []byte) (n int, err error) {
 	b.lastRead = opInvalid
 	m, ok := b.tryGrowByReslice(len(p))
@@ -192,9 +135,6 @@ func (b *ByteBuffer) Write(p []byte) (n int, err error) {
 	return copy(b.buf[m:], p), nil
 }
 
-// WriteString appends the contents of s to the buffer, growing the buffer as
-// needed. The return value n is the length of s; err is always nil. If the
-// buffer becomes too large, WriteString will panic with ErrTooLarge.
 func (b *ByteBuffer) WriteString(s string) (n int, err error) {
 	b.lastRead = opInvalid
 	m, ok := b.tryGrowByReslice(len(s))
@@ -204,10 +144,6 @@ func (b *ByteBuffer) WriteString(s string) (n int, err error) {
 	return copy(b.buf[m:], s), nil
 }
 
-// MinRead is the minimum slice size passed to a Read call by
-// ByteBuffer.ReadFrom. As long as the ByteBuffer has at least MinRead bytes beyond
-// what is required to hold the contents of r, ReadFrom will not grow the
-// underlying buffer.
 const MinRead = 1 << 9
 
 const MaxRead = MinRead << 8
@@ -242,16 +178,10 @@ func (b *ByteBuffer) ReadFrom(r io.Reader) (n int64, err error) {
 	}
 }
 
-// makeSlice allocates a slice of size n. If the allocation fails, it panics
-// with ErrTooLarge.
 func makeSlice(n int) *[]byte {
 	return GetBytes(n)
 }
 
-// WriteTo writes data to w until the buffer is drained or an error occurs.
-// The return value n is the number of bytes written; it always fits into an
-// int, but it is int64 to match the io.WriterTo interface. Any error
-// encountered during the write is also returned.
 func (b *ByteBuffer) WriteTo(w io.Writer) (n int64, err error) {
 	b.lastRead = opInvalid
 	if nBytes := b.Len(); nBytes > 0 {
@@ -270,15 +200,10 @@ func (b *ByteBuffer) WriteTo(w io.Writer) (n int64, err error) {
 			return n, io.ErrShortWrite
 		}
 	}
-	// ByteBuffer is now empty; reset.
 	b.Reset()
 	return n, nil
 }
 
-// Read reads the next len(p) bytes from the buffer or until the buffer
-// is drained. The return value n is the number of bytes read. If the
-// buffer has no data to return, err is io.EOF (unless len(p) is zero);
-// otherwise it is nil.
 func (b *ByteBuffer) Read(p []byte) (n int, err error) {
 	b.lastRead = opInvalid
 	if b.empty() {
--- bytes/types.go
+++ bytes/types.go
@@ -10,27 +10,58 @@ package gxbytes
 import "io"
 
 type Buffer interface {
+	// Read reads the next len(p) bytes from the buffer or until the buffer
+	// is drained. The return value n is the number of bytes read. If the
+	// buffer has no data to return, err is io.EOF (unless len(p) is zero);
+	// otherwise it is nil.
 	Read(p []byte) (n int, err error)
 
+	// ReadFrom reads data from r until EOF and appends it to the buffer, growing
+	// the buffer as needed. The return value n is the number of bytes read. Any
+	// error except io.EOF encountered during the read is also returned. If the
+	// buffer becomes too large, ReadFrom will panic with ErrTooLarge.
 	ReadFrom(r io.Reader) (n int64, err error)
 
+	// Write appends the contents of p to the buffer, growing the buffer as
+	// needed. The return value n is the length of p; err is always nil. If the
+	// buffer becomes too large, Write will panic with ErrTooLarge.
 	Write(p []byte) (n int, err error)
 
+	// WriteTo writes data to w until the buffer is drained or an error occurs.
+	// The return value n is the number of bytes written; it always fits into an
+	// int, but it is int64 to match the io.WriterTo interface. Any error
+	// encountered during the write is also returned.
 	WriteTo(w io.Writer) (n int64, err error)
 
+	// WriteString appends the contents of s to the buffer, growing the buffer as
+	// needed. The return value n is the length of s; err is always nil. If the
+	// buffer becomes too large, WriteString will panic with ErrTooLarge.
 	WriteString(s string) (n int, err error)
 
+	// Bytes returns a slice of length b.Len() holding the unread portion of the buffer.
+	// The slice is valid for use only until the next buffer modification (that is,
+	// only until the next call to a method like Read, Write, Reset, or Truncate).
+	// The slice aliases the buffer content at least until the next buffer modification,
+	// so immediate changes to the slice will affect the result of future reads.
 	Bytes() []byte
 
+	//set read index
 	ReadIndex(int)
 
+	// Len returns the number of bytes of the unread portion of the buffer;
+	// b.Len() == len(b.Bytes()).
 	Len() int
 
+	// Cap returns the capacity of the buffer's underlying byte slice, that is, the
+	// total space allocated for the buffer's data.
 	Cap() int
 
+	// init
 	Init(interface{})
 
+	// free resource
 	Free()
 
+	// name
 	Name() string
 }
